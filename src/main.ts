6fe50345bb21fcb1122eeb114e7a90488520dcbf
LogicHoward <1470247357@qq.com>
2024-08-01 16:55:40 +0800

    feat: :sparkles: 添加 element plus

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
-
 // 导入重置样式
 import './styles/reset.css'
 
@@ -13,5 +14,6 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
